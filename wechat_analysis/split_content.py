f5d7c90ba80e3a20b1158bc668c3c6541a36a4e1
HelloAtilol <41175733+HelloAtilol@users.noreply.github.com>
2018-11-01 17:25:53 +0800

    up__11.1

--- wechat_analysis/split_content.py
+++ wechat_analysis/split_content.py
@@ -8,17 +8,18 @@ date: 2018/10/30
 from ConnectDatabase import MySQLCommand
 import re
 
+at_dict = {}
+
 def order_content(conn):
     last_sid = ''
     title_list = ['createTime', 'msgSvrId', 'content']
     result = conn.select_order(title_list)
-    at_dict = {}
     f = open('data/content.txt', 'w', encoding='utf-8')
     for res in result:
         # 去除重复
         if res[1] != last_sid:
             content = res[2]
-            only_content, only_at = get_id_content(content)
+            only_content = get_id_content(content)
             f.write(only_content + '\n')
             last_sid = res[1]
     f.close()
@@ -28,18 +29,23 @@ def get_id_content(content):
     re_con = re.compile(r'^(.*)(:\n)(.*)')
     re_at = re.compile(r'^(.*?)(@)(.*)([\?\s]+)')
     id_content = re_con.match(content)
-    only_at = ''
+    global at_dict
     try:
         only_content = content.replace(id_content.group(1)+id_content.group(2), '')
         if '<' in only_content:
-            return only_content, ''
+            return only_content
         at_content = re_at.match(only_content)
         if at_content is not None:
-            only_at = at_content.group(2)
+            only_at = at_content.group(3)
             only_content = only_content.replace(at_content.group(0), '')
+            if only_at not in at_dict:
+                at_dict[only_at] = 1
+            else:
+                at_dict[only_at] += 1
+            print(only_at)
     except AttributeError:
         only_content = ''
-    return only_content, only_at
+    return only_content
 
 
 def main():
